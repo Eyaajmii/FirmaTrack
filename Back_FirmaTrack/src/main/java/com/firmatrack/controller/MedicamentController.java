783ba0643ab4543efaa42521460a9cbf1703b161
Mariem <jlassiimariem52@gmail.com>
2026-04-29 22:46:56 +0100

    feat: [US-49,US-50,US-51,US-52,US-55] gestion stock et intégration santé

--- Back_FirmaTrack/src/main/java/com/firmatrack/controller/MedicamentController.java
+++ Back_FirmaTrack/src/main/java/com/firmatrack/controller/MedicamentController.java
@@ -1,7 +1,6 @@
 package com.firmatrack.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +14,18 @@ public class MedicamentController {
 
     @Autowired
     private MedicamentService medicamentService;
-    @PostMapping("/traitement/{traitementId}")
-    public ResponseEntity<?> ajouterMedicament(@PathVariable Long traitementId,
-            @RequestBody Medicament m) {
 
-        Medicament saved = medicamentService.ajouterMedicament(traitementId, m);
-
-        if (saved == null) {
-            return ResponseEntity.badRequest()
-                    .body("Traitement introuvable");
+    // UNIQUE MÉTHODE D'AJOUT (Celle qui gère le stock)
+    @PostMapping("/traitement/{traitementId}/stock/{stockId}")
+    public ResponseEntity<?> ajouterMedicament(@PathVariable Long traitementId, @PathVariable Long stockId, @RequestBody Medicament m) {
+        try {
+            return ResponseEntity.ok(medicamentService.ajouterMedicament(traitementId, stockId, m));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-
-        return ResponseEntity.ok(saved);
     }
+
+
     @PutMapping("/{id}")
     public ResponseEntity<Medicament> updateMedicament(@PathVariable Long id, @RequestBody Medicament m) {
         Medicament updated = medicamentService.updateMedicament(id, m);
@@ -36,29 +34,29 @@ public class MedicamentController {
         }
         return ResponseEntity.ok(updated);
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMedicament(@PathVariable Long id) {
         medicamentService.deleteMedicament(id);
         return ResponseEntity.noContent().build();
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Medicament> getById(@PathVariable Long id) {
         Medicament m = medicamentService.getMedicamentById(id);
-
         if (m == null) {
             return ResponseEntity.notFound().build();
         }
-
         return ResponseEntity.ok(m);
     }
+
     @GetMapping
     public ResponseEntity<List<Medicament>> getAll() {
         return ResponseEntity.ok(medicamentService.getAllMedicaments());
     }
+
     @GetMapping("/traitement/{traitementId}")
     public ResponseEntity<List<Medicament>> getByTraitement(@PathVariable Long traitementId) {
-        return ResponseEntity.ok(
-                medicamentService.getByTraitement(traitementId)
-        );
+        return ResponseEntity.ok(medicamentService.getByTraitement(traitementId));
     }
 }
